aee595dee9ab5c82d3afe2103e6fb7c78fa87c91
Floriansylvain <sylvainflorian@proton.me>
2025-12-23 23:14:04 +0100

    feat: primevue base

---
+++ web/src/components/NavComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Button, Drawer } from 'primevue'
+
+const visible = ref(false)
+</script>
+
+<template>
+	<header>
+		<Drawer v-model:visible="visible">
+			<template #container="{ closeCallback }">
+				<div class="button-position">
+					<Button
+						type="button"
+						@click="closeCallback"
+						icon="pi pi-times"
+						variant="outlined"
+					></Button>
+				</div>
+			</template>
+		</Drawer>
+	</header>
+	<div class="button-position">
+		<Button icon="pi pi-bars" @click="visible = true" />
+	</div>
+</template>
+
+<style scoped></style>
